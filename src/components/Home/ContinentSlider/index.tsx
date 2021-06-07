812cf8d22485e4f602bd2b7a2bde6f44eb4425bb
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-06-06 23:38:07 -0300

    Create a component to the continent list

---
+++ src/components/Home/ContinentSlider/index.tsx
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+import { Box, chakra, Heading, Text } from '@chakra-ui/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
+import styles from './ContinentSlider.module.scss';
+
+interface ContinentProps {
+  id: string;
+  slug: string;
+  name: string;
+  description: string;
+  banner: {
+    url: string;
+  };
+}
+
+interface ContinentList {
+  continents: ContinentProps[];
+}
+
+export default function ContinentSlider({
+  continents,
+}: ContinentList): JSX.Element {
+  return (
+    <Box width="100%" maxWidth="1120px" margin="0 auto">
+      <Swiper
+        navigation
+        pagination
+        loop
+        autoplay={{
+          delay: 4000,
+        }}
+        className={styles.continentSlider}
+      >
+        {continents.map(continent => (
+          <SwiperSlide key={continent.id}>
+            <chakra.figure width="100%" display="block" position="relative">
+              <chakra.div
+                position="absolute"
+                top="0"
+                right="0"
+                bottom="0"
+                left="0"
+                backgroundColor="rgba(0, 0, 0, 0.3)"
+                display="flex"
+                flexDir="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Link key={continent.id} href={`/continent/${continent.slug}`}>
+                  <a>
+                    <Heading
+                      as="h3"
+                      color="#ffffff"
+                      textAlign="center"
+                      size="2xl"
+                    >
+                      {continent.name}
+                    </Heading>
+                    <Text color="#ffffff" textAlign="center" fontSize="xl">
+                      {continent.description}
+                    </Text>
+                  </a>
+                </Link>
+              </chakra.div>
+              <chakra.img
+                src={continent.banner.url}
+                alt={continent.name}
+                width="100%"
+                height="420"
+                objectFit="cover"
+              />
+            </chakra.figure>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
+import { Box, Heading } from '@chakra-ui/react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper/core';
+
 import Header from '../components/Header';
 import Interests from '../components/Home/Interests';
 import WelcomeBanner from '../components/Home/WelcomeBanner';
 
 import { request } from '../services/datocms.js';
+import ContinentSlider from '../components/Home/ContinentSlider';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 interface BannerProps {
   title: string;
@@ -18,12 +25,27 @@ interface InterestProps {
     url: string;
   };
 }
+
+interface ContinentProps {
+  id: string;
+  name: string;
+  slug: string;
+  description: string;
+  banner: {
+    url: string;
+  };
+}
 interface HomeProps {
   banner: BannerProps;
   interests: InterestProps[];
+  continents: ContinentProps[];
 }
 
-export default function Home({ banner, interests }: HomeProps): JSX.Element {
+export default function Home({
+  banner,
+  interests,
+  continents,
+}: HomeProps): JSX.Element {
   return (
     <>
       <Head>
@@ -32,6 +54,44 @@ export default function Home({ banner, interests }: HomeProps): JSX.Element {
       <Header />
       <WelcomeBanner title={banner.title} subtitle={banner.subtitle} />
       <Interests interests={interests} />
+      <Box mt={20} pt={14}>
+        <Heading
+          as="h4"
+          textAlign="center"
+          fontSize="3xl"
+          position="relative"
+          mb={14}
+          _before={{
+            position: 'absolute',
+            content: '""',
+            width: '90px',
+            height: '2px',
+            backgroundColor: '#000',
+            top: '-50px',
+            left: '50%',
+            marginLeft: '-45px',
+          }}
+        >
+          Let's go?
+          <br />
+          Choose a continent.
+        </Heading>
+
+        <ContinentSlider continents={continents} />
+      </Box>
+
+      <Box
+        as="footer"
+        mt={16}
+        p={4}
+        borderTopWidth="1px"
+        borderTopStyle="solid"
+        borderTopColor="gray.200"
+      >
+        <Box width="100%" maxWidth="1120px" margin="0 auto">
+          rodapé
+        </Box>
+      </Box>
     </>
   );
 }
@@ -48,18 +108,21 @@ export const getStaticProps: GetStaticProps = async () => {
 
   // interest items
 
-  const interestResponse = await request(`{
-    allInterests(orderBy: _createdAt_ASC) {
-      id
-      name
-      icon {
-        url
-      }
-    }
-  }
-  `);
+  const interestList = await request(
+    `{ allInterests(orderBy: _createdAt_ASC) { id name icon { url } } } `,
+  );
+
+  // continents
+
+  const continentList = await request(
+    `{ allContinents { id slug name description banner { url } } }`,
+  );
 
   return {
-    props: { banner, interests: interestResponse.allInterests },
+    props: {
+      banner,
+      interests: interestList.allInterests,
+      continents: continentList.allContinents,
+    },
   };
 };
