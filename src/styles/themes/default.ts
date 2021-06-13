0869afa40e8ce3c0abba78f2287b49a6d058b52d
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-06-13 11:44:23 -0300

    Displaying information about the continent and its most visited cities

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -3,6 +3,13 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = {
+  styles: {
+    global: props => ({
+      body: {
+        background: props.colorMode === 'light' ? 'white' : 'gray.800',
+      },
+    }),
+  },
   fonts: {
     body: 'Titillium Web, Helvetica, Arial, sans-serif',
     heading: 'Titillium Web, Helvetica, Arial, sans-serif',
@@ -13,6 +20,9 @@ const theme = {
     regular: 400,
     semibold: 500,
     bold: 700,
+  },
+  colors: {
+    primary: '#ffba08',
   },
 };
 
